80bb27c9c647be4132d9a8181a4f81f3ac620d7d
E-MRE <gltkr.emre@gmail.com>
2021-08-16 23:15:08 +0300

    YaZILIM Geliştirme Kampı 13. Ders

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Abstract;
+using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -10,47 +12,56 @@ using Entities.DTOs;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
-        private IProductDal productDal;
+        private IProductDal _productDal;
+        private ICategoryService _categoryService;
 
-        public ProductManager(IProductDal productDal)
+        public ProductManager(IProductDal productDal, ICategoryService categoryService)
         {
-            this.productDal = productDal;
+            _productDal = productDal;
+            _categoryService = categoryService;
         }
 
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            productDal.Add(product);
+            var result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName),
+                CheckIfProductCountOfCategoryCorrect(product.CategoryId), CheckIfCategoryLimitExceded());
+
+            if (result != null)
+                return result;
+
+            _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
-        
+
         public IDataResult<List<Product>> GetAll()
         {
             if (DateTime.Now.Hour == 1)
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
 
-            return new SuccessDataResult<List<Product>>(productDal.GetAll(), Messages.ProductsListed);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return new SuccessDataResult<List<Product>>(productDal.GetAll(p => p.CategoryId == id));
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
         }
 
         public IDataResult<List<Product>> GetAllByUnitPrice(decimal min, decimal max)
         {
-            return new SuccessDataResult<List<Product>>(productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
         public IDataResult<Product> GetById(int productId)
         {
-            return new SuccessDataResult<Product>(productDal.Get(p => p.ProductId == productId));
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
@@ -58,7 +69,42 @@ namespace Business.Concrete
             if (DateTime.Now.Hour == 21)
                 return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
 
-            return new SuccessDataResult<List<ProductDetailDto>>(productDal.GetProductDetails());
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
+        }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            _productDal.Update(product);
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+        {
+            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result >= 15)
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductNameExists(string productName)
+        {
+            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+
+            if (result)
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
+
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count > 15)
+                return new ErrorResult(Messages.CategoryLimitExceded);
+
+            return new SuccessResult();
         }
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,8 @@ namespace Business.Constants
         public const string ProductNameInvalid = "Ürün ismi geçersiz";
         public const string MaintenanceTime = "Sistem Bakımda";
         public const string ProductsListed = "Ürünler listelendi";
+        public const string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir.";
+        public const string ProductNameAlreadyExists = "Böyle bir ürün zaten mevcut";
+        public const string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
