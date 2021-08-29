055d29469375284990d099ce676059779ed1c22e
E-MRE <gltkr.emre@gmail.com>
2021-08-29 13:46:15 +0300

    Yazılım Geliştirme Kampı 14. Ders

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -13,5 +13,12 @@ namespace Business.Constants
         public const string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir.";
         public const string ProductNameAlreadyExists = "Böyle bir ürün zaten mevcut";
         public const string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public const string AuthorizationDenied = "Yetkiniz yok.";
+        public const string UserRegistered = "Kayıt oldu.";
+        public const string UserNotFound = "Kullanıcı bulunamadı.";
+        public const string PasswordError = "Hatalı şifre.";
+        public const string SuccessfulLogin = "Giriş başarılı.";
+        public const string UserAlreadyExists = "Kullanıcı zaten mevcut.";
+        public const string AccessTokenCreated = "Access token oluşturuldu.";
     }
 }
---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA256())
+            {
+                passwordSalt = GetCustomSaltKey(hmac.Key);
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA256(GetCustomSaltKey(passwordSalt)))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static byte[] GetCustomSaltKey(byte[] passwordSalt)
+        {
+            var firstSalt = Encoding.UTF8.GetBytes("EMRE");
+            var lastSalt = Encoding.UTF8.GetBytes("GLTKR");
+
+            List<byte> saltList = new List<byte>();
+            saltList.AddRange(firstSalt);
+            saltList.AddRange(passwordSalt);
+            saltList.AddRange(lastSalt);
+
+            return saltList.ToArray();
+        }
+    }
+}
